8aa3147ad070de791b6002a2bf62e6b172778160
Adam <admbaum@protonmail.com>
2024-09-02 21:59:53 -0400

    yolo'd some new things

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,9 +6,21 @@ class MainPage(BasePage):
     SEARCH_INPUT = (By.ID, "searchInput")
     SEARCH_BUTTON = (By.CSS_SELECTOR, "button[type='submit']")
     SEARCH_RESULTS = (By.ID, "firstHeading")  # assuming this is the locator for the first search result
+    LANGUAGE_DROPDOWN = (By.ID, "searchLanguage")
+    MAIN_HEADING = (By.ID, "mp-welcome")
 
     def enter_search_query(self, query):
         self.enter_text(self.SEARCH_INPUT, query)
 
     def click_search_button(self):
         self.click_element(self.SEARCH_BUTTON)
+
+    def select_language(self, language):
+        self.select_dropdown_option(self.LANGUAGE_DROPDOWN, language)
+
+    def select_dropdown_option(self, locator, option_text):
+        dropdown = self.find_element(locator)
+        for option in dropdown.find_elements(By.TAG_NAME, "option"):
+            if option.text == option_text:
+                option.click()
+                break
--- tests/test_wikipedia_search.py
+++ tests/test_wikipedia_search.py
@@ -1,17 +1,30 @@
 from pages.main_page import MainPage
 
+class TestWikipedia:
+    def test_wikipedia_search(self, driver):
+        # Arrange
+        driver.get("https://www.wikipedia.org/")
+        main_page = MainPage(driver)
 
-def test_wikipedia_search(driver):
-    driver.get("https://www.wikipedia.org/")
-    main_page = MainPage(driver)
-    main_page.enter_search_query("Python (programming language)")
-    main_page.click_search_button()
+        # Act
+        main_page.enter_search_query("Python (programming language)")
+        main_page.click_search_button()
 
-    # Assertions
-    search_results_page = main_page.find_element(
-        main_page.SEARCH_RESULTS)  # Replace with the actual locator for search results
-    assert search_results_page.is_displayed()
-    assert "Python (programming language)" in driver.title
+        # Assert
+        search_results_page = main_page.find_element(
+            main_page.SEARCH_RESULTS)  # Replace with the actual locator for search results
+        assert search_results_page.is_displayed()
+        assert "Python (programming language)" in driver.title
 
+    def test_wikipedia_language_selection(self, driver):
+        # Arrange
+        driver.get("https://www.wikipedia.org/")
+        main_page = MainPage(driver)
 
+        # Act
+        main_page.select_language("Español")
 
+        # Assert
+        assert "Wikipedia" in driver.title
+        assert driver.current_url == "https://es.wikipedia.org/wiki/Wikipedia:Portada"
+        assert main_page.get_text(main_page.MAIN_HEADING) == "Bienvenidos a Wikipedia,"
